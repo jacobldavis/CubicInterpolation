e7cfd7584fb5266facc1a3f081c3f7fff1a17e76
Jacob Davis <jldcraft@gmail.com>
2025-06-07 10:24:15 -0400

    updated eigen implementation

--- cpp_test/cpp_frame/cubic_interp_eigen.cpp
+++ cpp_test/cpp_frame/cubic_interp_eigen.cpp
@@ -89,7 +89,10 @@ Eigen::VectorXd cubic_interp_eval_eigen(const cubic_interp* interp, Eigen::Vecto
     Eigen::VectorXd ix = t.array().floor();
     t -= ix;
 
-    std::vector<int> ai(ix.data(), ix.data() + ix.rows() * ix.cols());
+    Eigen::MatrixXd ai(t.size(), 4);
+    for (Eigen::Index i = 0; i < t.size(); ++i) {
+        ai.row(i) = interp->a.row(static_cast<Eigen::Index>(ix(i)));
+    }
 
-    return (t.array() * (t.array() * (t.array() * interp->a(ai, 0).array() + interp->a(ai, 1).array()) + interp->a(ai, 0).array()) + interp->a(ai, 0).array()).matrix();
+    return (t.array() * (t.array() * (t.array() * ai.col(0).array() + ai.col(1).array()) + ai.col(2).array()) + ai.col(3).array()).matrix();
 }
