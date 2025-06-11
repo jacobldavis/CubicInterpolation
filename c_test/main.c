3e8f08016fd33571845ac5f3632007d5f91412b2
Jacob Davis <jldcraft@gmail.com>
2025-06-11 10:27:42 -0400

    updated c data collection

--- c_test/main.c
+++ c_test/main.c
@@ -32,20 +32,22 @@ int main(int argc, char **argv) {
     double **onevalues = read_1dvalues();
     double ***twovalues = read_2dvalues();
     FILE* cfp = fopen("c_data.csv", "w");
+    FILE* bicfp = fopen("bi_c_data.csv", "w");
     FILE* cudafp = fopen("cuda_data.csv", "w");
     FILE* clfp = fopen("cl_data.csv", "w");
 
     // Executes the tests for onevalues and two values
     srand(time(NULL));
     test_all_cubic(onevalues, cfp);
-    test_all_bicubic(twovalues, cfp);
+    test_all_bicubic(twovalues, bicfp);
     test_all_cubic_cuda(onevalues, cudafp);
     test_all_cubic_cl(onevalues, clfp);
 
-    // Frees onevalues and twovalues
+    // Frees onevalues and twovalues and closes files
     free1d(onevalues);
     free2d(twovalues);
     fclose(cfp);
+    fclose(bicfp);
     fclose(cudafp);
     fclose(clfp);
 
